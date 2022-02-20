1196fb20423935b723d3cf2b2c9147f829d47b29
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-02-20 19:25:24 +0530

    added url for admin endpoint

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('ticket/', views.ticket, name='ticket')
+    path('ticket/', views.ticket, name='ticket'),
+    path('admin/', views.admin, name='admin')
 ]
