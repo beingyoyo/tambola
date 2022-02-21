d751bf1483c7ebc8b231fcda1bcdeca34ca15c9d
Yash Makwana <37649009+beingyoyo@users.noreply.github.com>
2022-02-21 18:05:49 +0530

    final changes

--- tambola/settings.py
+++ tambola/settings.py
@@ -117,11 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_heroku
-django_heroku.settings(locals())
